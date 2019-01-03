3248da18f2ca31958d15fde146293c6a672f2930
alexander.kim <kim.alexander.ca@gmail.com>
2019-01-03 14:29:35 -0500

    static router update

--- staticRouter.php
+++ staticRouter.php
@@ -13,7 +13,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
-
 $fileLocator = new FileLocator(array(__DIR__.'/conf'));
 $loader = new YamlFileLoader($fileLocator);
 $routes = $loader->load('routes.yaml');
@@ -24,23 +23,21 @@ $matcher = new UrlMatcher($routes, $context);
 //die(print_r($_SERVER));
 try {
     $parameters = $matcher->match($_SERVER['REQUEST_URI']);
-    $t = new \Vanilla\Knowledge\Controllers\Api\KnowledgeBasesApiController();
-    //die(var_dump($t));
-    //return call_user_func_array($parameters['_controller'], []);
-    //(print_r($parameters));
-    $controller = new $parameters['class']();
-    $str = $controiller->$parameters['method'];
-    //var_dump($controller);
-    //echo '!'.$str;
-    //return call_user_func_array($parameters['_controller'], []);
-
+    require_once(__DIR__.'/container.php');
+    //die(var_dump($parameters));
+    $controller = Gdn::factory($parameters['_controller']);
+    $data = $controller->{$parameters['action']}();
+//    die(var_dump($data));
+    $response = new \Garden\Web\Data((array)$data);
+    $response->render();
+    exit;
 } catch (ResourceNotFoundException $e) {
     // just do nothing and pass control to Gdn system
     // which is bootstrap.php
 } catch (Throwable $e) {
-    //die($e->getMessage());
+    // we should implement some handler here
+    // this is the case when static route was detected,
+    // but some exception was fired and not handled by controller itself
+    die($e->getMessage());
 }
 
-//$routes = new RouteCollection();
-
-//$route = new Route('/foo', array('_controller' => 'MyController'));
